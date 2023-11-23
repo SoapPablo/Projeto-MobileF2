b495a39dd7a626f21da8fcf5f5059a54d48b8d0e
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2023-11-23 17:23:22 -0300

    "Text Imput" adicionados colse #6

--- screens/CriarEventos.js
+++ screens/CriarEventos.js
@@ -1,5 +1,6 @@
 import MyImagePicker from '../components/MyImagePicker';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 const CriarEventos = () => {
   return (
@@ -7,6 +8,31 @@ const CriarEventos = () => {
       <View>
         <View style={styles.image}>
           <MyImagePicker onImageSelected={(uri) => setImagemEvento(uri)} />
+        </View>
+        <View style={styles.container}>
+          <TextInput
+            style={styles.textinput}
+            type="outlined"
+            label="Nome do evento"
+          />
+
+          <TextInput
+            style={styles.textinput}
+            type="outlined"
+            label="Subtítulo (opcional)"
+          />
+
+          <TextInput
+            style={styles.textinputdesc}
+            label="Descrição do evento (opcional)"
+            multiline={true}
+            numberOfLines={10}
+          />
+          <TextInput
+            style={styles.textinput2}
+            type="outlined"
+            label="Endereço"
+          />
         </View>
       </View>
     </ScrollView>
@@ -22,7 +48,35 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: 270,
-  }
+  },
+  container: {
+    flex: 1,
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textinput: {
+    margin: 10,
+    borderRadius: 0,
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
+    width: '90%',
+  },
+  textinput2: {
+    margin: 10,
+    borderRadius: 0,
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
+    width: '90%',
+  },
+  textinputdesc: {
+    margin: 10,
+    borderRadius: 0,
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
+    height: 200,
+    width: '90%',
+  },
 });
 
 export default CriarEventos;
