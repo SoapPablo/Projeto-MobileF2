98f1670b0986f3a18da3483aabf8165b50de663f
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2023-11-23 17:01:55 -0300

    Adicionando image picker close #4

--- screens/CriarEventos.js
+++ screens/CriarEventos.js
@@ -1,7 +1,28 @@
+import MyImagePicker from '../components/MyImagePicker';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+
 const CriarEventos = () => {
   return (
-    <></>
+    <ScrollView contentContainerStyle={styles.scrollView}>
+      <View>
+        <View style={styles.image}>
+          <MyImagePicker onImageSelected={(uri) => setImagemEvento(uri)} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    paddingBottom: 20,
+  },
+  image: {
+    width: '100%',
+    height: 270,
+  }
+});
 
 export default CriarEventos;
