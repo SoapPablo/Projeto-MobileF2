1f47859e7b32c7ec9b5765ae8abfed40c32e2f36
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2023-11-27 17:41:47 -0300

    Packzão bolado cheio de updates 😎👍

--- screens/BuscarEventos.js
+++ screens/BuscarEventos.js
@@ -5,44 +5,32 @@ import {
   Image,
   StyleSheet,
   ScrollView,
-  Button,
+  TouchableOpacity,
 } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { useEventoContext } from '../contexts/EventoContext';
-import imagem1 from '../assets/ImgPicker.png';
 
-const EventCard = () => (
-  <View style={styles.card}>
-    <Image source={{imagem1}} style={styles.image} />
+const EventCard = ({
+  imagemEvento,
+  nomeEvento,
+  dataEvento,
+  horaEvento,
+  subtitulo,
+  onPress,
+}) => (
+  <TouchableOpacity style={styles.card} onPress={onPress}>
+    <Image source={{ uri: imagemEvento }} style={styles.image} />
     <View style={styles.content}>
-      <Text style={styles.title}>Churrascada PGT</Text>
+      <Text style={styles.title}>{nomeEvento}</Text>
       <Text style={styles.details}>
-        Data: 18hrs | Horário: 19hrs
+        Data: {dataEvento} | Horário: {horaEvento}
       </Text>
       <Text style={styles.description} numberOfLines={3}>
-        LKJHBGFDGHJKLKKOIUYTRFGVHBNKMLOI987Y6T5RTFGVHBJNKMLOIU87Y6T5RTF
+        {subtitulo}
       </Text>
     </View>
-  </View>
+  </TouchableOpacity>
 );
-
-// apagar este event card de cima e descomentar este de de baixo !!!!!!!!!!!!!!!!!!!!!
-
-
-// const EventCard = () => (
-//   <View style={styles.card}>
-//     <Image source={imageSource} style={styles.image} />
-//     <View style={styles.content}>
-//       <Text style={styles.title}>{title}</Text>
-//       <Text style={styles.details}>
-//         Data: {date} | Horário: {time}
-//       </Text>
-//       <Text style={styles.description} numberOfLines={3}>
-//         {description}
-//       </Text>
-//     </View>
-//   </View>
-// );
 
 const BuscarEventos = ({ navigation }) => {
   const { eventos } = useEventoContext();
@@ -57,12 +45,30 @@ const BuscarEventos = ({ navigation }) => {
         onChangeText={onChangeSearch}
         value={searchQuery}
       />
-      <EventCard/> 
-      
-      {/* {eventos.map((evento, index) => (
-        <EventCard key={index} {...evento} />
-      ))} */}
-      <Button theme={{ colors: { primary: "#9400d3" } }} title="Criar Eventos" onPress={() => navigation.navigate('Criar Eventos')} />
+
+      {eventos.length > 0 ? (
+        eventos.map((evento) => (
+          <EventCard
+            key={evento.id}
+            imagemEvento={evento.imagemEvento}
+            nomeEvento={evento.nomeEvento}
+            dataEvento={evento.dataEvento}
+            horaEvento={evento.horaEvento}
+            subtitulo={evento.subtitulo}
+            onPress={() =>
+              navigation.navigate('Detalhes do Evento', { eventoId: evento.id })
+            }
+          />
+        ))
+      ) : (
+        <Text style={styles.noEventsText}>Nenhum evento encontrado</Text>
+      )}
+
+      <TouchableOpacity
+        style={styles.createEventButton}
+        onPress={() => navigation.navigate('Criar Eventos')}>
+        <Text style={styles.createEventButtonText}>Criar Evento</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
@@ -70,10 +76,11 @@ const BuscarEventos = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
   },
   card: {
     flexDirection: 'row',
-    backgroundColor: 'purple',
+    backgroundColor: '#fff',
     borderRadius: 8,
     marginBottom: 10,
     padding: 10,
@@ -82,13 +89,13 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 2,
     elevation: 3,
-    marginHorizontal: 10, // Margem horizontal para alinhar com o Appbar
   },
   image: {
     width: 100,
     height: 'auto',
     resizeMode: 'cover',
     marginRight: 10,
+    borderRadius: 8,
   },
   content: {
     flex: 1,
@@ -97,16 +104,34 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 5,
-    color: 'white',
+    color: '#333',
   },
   details: {
     fontSize: 14,
-    color: '#fff',
+    color: '#555',
     marginBottom: 5,
   },
   description: {
     fontSize: 16,
-    color: 'white',
+    color: '#666',
+  },
+  noEventsText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 18,
+    color: 'gray',
+  },
+  createEventButton: {
+    backgroundColor: '#9400d3',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  createEventButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
